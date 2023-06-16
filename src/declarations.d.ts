bd765d2aa4016d85e63588b175681ee21bb96e98
soxhog <soranatsu.12250511@gmail.com>
2023-06-16 13:39:27 +0900

    init

---
+++ src/declarations.d.ts
@@ -0,0 +1,10 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    "a-scene": any;
+    // 他のA-Frame要素もここに追加できます
+    "a-marker": any;
+    "a-entity": any;
+    "a-assets": any;
+    "a-asset-item": any;
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,51 @@
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { useEffect } from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  //   useEffect(() => {
+  //     const script = document.createElement("script");
+  //     script.src = "https://aframe.io/releases/1.2.0/aframe.min.js";
+  //     script.async = true;
+  //     document.body.appendChild(script);
+
+  //     const script2 = document.createElement("script");
+  //     script2.src =
+  //       "https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js";
+  //     script2.async = true;
+  //     document.body.appendChild(script2);
+  //   }, []);
+
+  return (
+    <>
+      {/* <Head>
+        <title>AR Page</title>
+        <meta name="description" content="AR Page" />
+      </Head> */}
+      <a-scene
+        embedded
+        arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 3x3;"
+      >
+        <a-assets>
+          <a-asset-item id="kitsune" src="263.gltf"></a-asset-item>
+        </a-assets>
+        <a-marker
+          preset="custom"
+          type="pattern"
+          url="pattern-logo_A_bg_white.patt"
+        >
+          <a-entity
+            id="model"
+            gltf-model="#kitsune"
+            position="0 0 1"
+            scale="1 1 1"
+            rotation="0 -90 60"
+          ></a-entity>
+        </a-marker>
+        <a-entity camera></a-entity>
+      </a-scene>
+    </>
+  );
+}
